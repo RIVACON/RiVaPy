aa95340a739d9ca06a846743d570a0ffbe7a61b3
Daniel Oeltz <d.oeltz@rivacon.com>
2023-11-27 08:15:13 +0100

    admin

--- rivapy/models/gas_fwd_model.py
+++ rivapy/models/gas_fwd_model.py
@@ -1,5 +1,5 @@
 import abc
-from typing import Union, Callable, List, Set
+from typing import Union, Callable, List, Set, Tuple
 import numpy as np
 from rivapy.models.base_model import BaseFwdModel, ForwardSimulationResult
 
@@ -30,22 +30,41 @@ class GassFwdModel2Factor(BaseFwdModel):
             pass
 
 
-    def __init__(self,vol1: Union[float, Callable[], 
+    def __init__(self,
+                 vol1:Union[float,Tuple[List[float], List[float]]], 
                  alpha1: float, 
-                 vol2: float, 
-                 alpha2: float, 
+                 vol2: Union[float,Tuple[List[float], List[float]]], 
+                 alpha2: float,
+                 corr: float,
                  udl:str):
         self.udl = udl
         self.vol1 = vol1
         self.alpha1 = alpha1
         self.vol2 = vol2
         self.alpha2 = alpha2
+        self.corr = corr
     
     def rnd_shape(self, n_sims: int, n_timesteps: int)->tuple:
         return (n_timesteps-1, 2*n_sims)
 
-    def simulate(self, timegrid, 
+    def _step(self,t1: float, t2: float):
+        zeta1 = np.exp(2.0*self.alpha1*t2)/(2.0*self.alpha1) - np.exp(2.0*self.alpha1*t1)/(2.0*self.alpha1) 
+        zeta2 = np.exp(2.0*self.alpha2*t2)/(2.0*self.alpha2) - np.exp(2.0*self.alpha2*t1)/(2.0*self.alpha2) 
+        alpha12 = self.alpha1+self.alpha2
+        zeta12 = np.exp(2.0*alpha12*t2)/(2.0*alpha12) - np.exp(2.0*alpha12*t1)/(2.0*alpha12)
+        r = zeta12/np.sqrt(zeta1*zeta2)
+
+        return r, zeta1, zeta2
+
+
+    def simulate(self, timegrid: np.ndarray, 
                 rnd: np.ndarray,
                 expiries: List[float],
                 fwd0: List[float])->ForwardSimulationResult:
-        
+        result = np.zeros((rnd.shape[0], rnd.shape[1], len(expiries)))
+        for i in range(len(expiries)):
+            result[0,:,i] = fwd0[i]
+        for i in range(timegrid.shape[0]-1):
+            t1 = timegrid[i]
+            t2 = timegrid[i+1]
+            dt = t2-t1
