b21d74d6d42c20841ed6d20d2302e94d94b19050
FabienneRIVAPY <f.schmid@rivacon.com>
2024-03-01 11:15:29 +0100

    new template for analysis of simplified and idealized 1d neuralSDE model

---
+++ notebooks/models/neuralSDE_idealized.py
@@ -0,0 +1,36 @@
+# import -----------------------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import pandas as pd
+
+
+from typing import Sequence, Optional, Union
+
+import torch
+import tqdm
+from torch import nn
+from torch import optim
+from torch.distributions import Normal
+
+from collections import namedtuple
+
+import torchsde
+
+#for plotting
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+import scipy as scp
+import scipy.stats as ss
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+from mpl_toolkits import mplot3d
+from matplotlib import cm
+import scipy.special as scsp
+from scipy.integrate import quad
+from scipy.interpolate import RegularGridInterpolator
+
+from sys import exit
