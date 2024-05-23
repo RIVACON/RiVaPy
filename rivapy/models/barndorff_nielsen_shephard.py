d31175374fafaac36b0de1ab158305dd6a6dca42
FabienneRIVAPY <f.schmid@rivacon.com>
2024-05-23 11:25:23 +0200

    Characteristic Function for BNS

--- rivapy/models/barndorff_nielsen_shephard.py
+++ rivapy/models/barndorff_nielsen_shephard.py
@@ -70,6 +70,48 @@ class BNS(FactoryObject):
         
 
 
+    def _characteristic_func(self, xi, s0, v0, tau):
+            """Characteristic function needed internally to compute call prices with analytic formula.
+            """
+            ixi = 1j * xi
+            f1 = ixi*self.rho - 0.5*(xi**2 + ixi)*(1.-np.exp(-self.lmbda*tau))/self.lmbda
+            f2 = ixi*self.rho - 0.5*(xi**2 + ixi)/self.lmbda
+            A = - self.a * self.lmbda *self.rho*tau/(self.b-self.rho)
+            B = - 0.5*(xi**2 + ixi)*(1.-np.exp(-self.lmbda*tau))/self.lmbda
+            C = (self.b * np.log((self.b - f1)/(self.b - ixi*self.rho)) + f2*self.lmbda*tau)*self.a/(self.b-f2)
+            return np.exp(ixi * np.log(s0) + A + v0*B + C)
+        
+	    
+    def compute_call_price(self, s0: float, v0: float, K: Union[np.ndarray, float], ttm: Union[np.ndarray, float])->Union[np.ndarray, float]:
+        """Computes a call price for the Heston model via integration over characteristic function.
+		Args:
+			s0 (float): current spot
+			v0 (float): current variance
+			K (float): strike
+			ttm (float): time to maturity
+		"""
+        if isinstance(ttm, np.ndarray):
+            result = np.empty((ttm.shape[0], K.shape[0], ))
+            for i in range(ttm.shape[0]):
+				#for j in range(K.shape[0]):
+					#result[i,j] = self.call_price(s0,v0,K[j], tau[i])
+                result[i,:] = self.compute_call_price(s0,v0,K, ttm[i])
+            return result
+
+        def integ_func(xi, s0, v0, K, tau, num):
+            ixi = 1j * xi
+            if num == 1:
+                return (self._characteristic_func(xi - 1j, s0, v0, tau) / (ixi * self._characteristic_func(-1j, s0, v0, tau)) * np.exp(-ixi * np.log(K))).real
+            else:
+                return (self._characteristic_func(xi, s0, v0, tau) / (ixi) * np.exp(-ixi * np.log(K))).real
+
+        if ttm < 1e-3:
+            res = (s0-K > 0) * (s0-K)
+        else:
+            "Simplified form, with only one integration. "
+            h = lambda xi: s0 * integ_func(xi, s0, v0, K, ttm, 1) - K * integ_func(xi, s0, v0, K, ttm, 2)
+            res = 0.5 * (s0 - K) + 1/scipy.pi * scipy.integrate.quad_vec(h, 0, 500.)[0]  #vorher 500
+        return res
   
 
 
