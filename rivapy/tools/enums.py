6ff6bf60ed79358295abe28ec15e5221ba838515
hnguyen-RIVACON <h.nguyen@rivacon.com>
2025-07-02 15:08:54 +0200

    modified FRA specifications as a mix of pyvacon plus extended feature: if AMxBM FRA period given, use that instead.

--- rivapy/tools/enums.py
+++ rivapy/tools/enums.py
@@ -180,7 +180,7 @@ class Period(_MyEnum):
 
 @_unique
 class RollConvention(_MyEnum):
-   FOLLOWING = "Following"
+    FOLLOWING = "Following"
     MODIFIED_FOLLOWING = "ModifiedFollowing"
     MODIFIED_FOLLOWING_EOM = "ModifiedFollowingEOM"
     MODIFIED_FOLLOWING_BIMONTHLY = "ModifiedFollowingBimonthly"
@@ -188,18 +188,20 @@ class RollConvention(_MyEnum):
     MODIFIED_PRECEDING = "ModifiedPreceding"
     NEAREST = "Nearest"
     UNADJUSTED = "Unadjusted"
-   
+
+
 @_unique
 class DayCounterType(_MyEnum):
-    ACT_ACT = 'ActAct'
-    Act365Fixed = 'Act365Fixed'
-    ACT360 = 'Act360'
-    ThirtyU360 = '30U360'
-    ThirtyE360 = '30E360'
-    ACT252 = 'Act252'
-    Thirty360ISDA = '30360ISDA'
-    ActActICMA = 'ActActICMA'
-    
+    ACT_ACT = "ActAct"
+    Act365Fixed = "Act365Fixed"
+    ACT360 = "Act360"
+    ThirtyU360 = "30U360"
+    ThirtyE360 = "30E360"
+    ACT252 = "Act252"
+    Thirty360ISDA = "30360ISDA"
+    ActActICMA = "ActActICMA"
+
+
 @_unique
 class InflationInterpolation(_MyEnum):
     UNDEFINED = "UNDEFINED"
@@ -208,7 +210,7 @@ class InflationInterpolation(_MyEnum):
     CONSTANT = "CONSTANT"
 
 
-@_unique 
+@_unique
 class Sector(_MyEnum):
     UNDEFINED = "UNDEFINED"
     # BASIC_MATERIALS = 'BasicMaterials'
