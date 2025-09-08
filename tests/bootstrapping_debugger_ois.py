a5efcbbf4698f380da121148dc36b694de961e23
hnguyen-RIVACON <h.nguyen@rivacon.com>
2025-09-08 17:36:24 +0200

    modified curve optional argument of bootstrapper to correctly handle null case, which was leading to erroneous multiple bootstrap runs.

--- tests/bootstrapping_debugger_ois.py
+++ tests/bootstrapping_debugger_ois.py
@@ -481,7 +481,7 @@ zero_interp.insert(0, zero_interp[0])
 
 plt.plot(dates_final, zero_final, marker="^", label="bootstrapped")
 # plt.scatter(end_date_deposits, quotes_deposits)
-plt.plot(dates_new, zero_interp, label="interpolated")
+plt.plot(dates_new, zero_interp, label="bootstrapped - interpolated")
 
 plt.xlabel("year")
 plt.ylabel("zero rates")
@@ -506,270 +506,274 @@ plt.show()
 # ##########################################
 # # 3M deposit
 
-# # setting up depoist
-# # calculation date
-# ref_date = dt.datetime(2019, 8, 31)  # refdate = dt.datetime(2017, 8, 31)
+# setting up depoist
+# calculation date
+ref_date = dt.datetime(2019, 8, 31)  # refdate = dt.datetime(2017, 8, 31)
 
-# # start date of the accrual period with spot lag equal to 2 days
-# start_date = ref_date + dt.timedelta(days=2)
+# start date of the accrual period with spot lag equal to 2 days
+start_date = ref_date + dt.timedelta(days=2)
 
-# # end date of the accrual period is 1 day after startdate
-# end_date = start_date + relativedelta(months=3)
+# end date of the accrual period is 1 day after startdate
+end_date = start_date + relativedelta(months=3)
 
-# # specification of the deposit
-# ccy = "EUR"
-# dcc = "Act365Fixed"  # "Act360"normally a bond is ACT360, we keep consistency with the swaps
-# rate = 0.01
-# notional = 100.0
-# dep_3M = DepositSpecification(
-#     obj_id="3M_DEPOSIT",
-#     issuer="dummy_issuer",
-#     currency=ccy,
-#     fixing_date=ref_date,
-#     start_date=start_date,
-#     maturity_date=end_date,
-#     notional=notional,
-#     rate=rate,
-#     day_count_convention=dcc,
-# )
-# ##########################################
-# # 6M deposit
-# # start date of the accrual period with spot lag equal to 2 days
-# start_date = ref_date + dt.timedelta(days=2)
+# specification of the deposit
+ccy = "EUR"
+dcc = "Act365Fixed"  # "Act360"normally a bond is ACT360, we keep consistency with the swaps
+rate = 0.01
+notional = 100.0
+dep_3M = DepositSpecification(
+    obj_id="3M_DEPOSIT",
+    issuer="dummy_issuer",
+    currency=ccy,
+    fixing_date=ref_date,
+    start_date=start_date,
+    maturity_date=end_date,
+    notional=notional,
+    rate=rate,
+    day_count_convention=dcc,
+)
+##########################################
+# 6M deposit
+# start date of the accrual period with spot lag equal to 2 days
+start_date = ref_date + dt.timedelta(days=2)
 
-# # end date of the accrual period is 1 day after startdate
-# end_date = start_date + relativedelta(months=6)
+# end date of the accrual period is 1 day after startdate
+end_date = start_date + relativedelta(months=6)
 
-# # specification of the deposit
-# ccy = "EUR"
-# dcc = "Act365Fixed"  # "Act360"normally a bond is ACT360, we keep consistency with the swaps
-# rate = 0.01
-# notional = 100.0
-# dep_6M = DepositSpecification(
-#     obj_id="6M_DEPOSIT",
-#     issuer="dummy_issuer",
-#     currency=ccy,
-#     fixing_date=ref_date,
-#     start_date=start_date,
-#     maturity_date=end_date,
-#     notional=notional,
-#     rate=rate,
-#     day_count_convention=dcc,
-# )
-# ##########################################
-# # 9M deposit
-# # start date of the accrual period with spot lag equal to 2 days
-# start_date = ref_date + dt.timedelta(days=2)
+# specification of the deposit
+ccy = "EUR"
+dcc = "Act365Fixed"  # "Act360"normally a bond is ACT360, we keep consistency with the swaps
+rate = 0.01
+notional = 100.0
+dep_6M = DepositSpecification(
+    obj_id="6M_DEPOSIT",
+    issuer="dummy_issuer",
+    currency=ccy,
+    fixing_date=ref_date,
+    start_date=start_date,
+    maturity_date=end_date,
+    notional=notional,
+    rate=rate,
+    day_count_convention=dcc,
+)
+##########################################
+# 9M deposit
+# start date of the accrual period with spot lag equal to 2 days
+start_date = ref_date + dt.timedelta(days=2)
 
-# # end date of the accrual period is 1 day after startdate
-# end_date = start_date + relativedelta(months=9)
+# end date of the accrual period is 1 day after startdate
+end_date = start_date + relativedelta(months=9)
 
-# # specification of the deposit
-# ccy = "EUR"
-# dcc = "Act365Fixed"  # "Act360"normally a bond is ACT360, we keep consistency with the swaps
-# rate = 0.01
-# notional = 100.0
-# dep_9M = DepositSpecification(
-#     obj_id="9M_DEPOSIT",
-#     issuer="dummy_issuer",
-#     currency=ccy,
-#     fixing_date=ref_date,
-#     start_date=start_date,
-#     maturity_date=end_date,
-#     notional=notional,
-#     rate=rate,
-#     day_count_convention=dcc,
-# )
-# ##########################################
-# # 1Y IRS
-# # 1Y maturity 3M swap, i.e. the floating leg is reset every 3M
-# # start dates of the accrual periods corresponding to the tenor of the underlying index (3 months). The spot lag is set to 0.
-# start_dates = [ref_date + relativedelta(months=3 * i) for i in range(4)]
+# specification of the deposit
+ccy = "EUR"
+dcc = "Act365Fixed"  # "Act360"normally a bond is ACT360, we keep consistency with the swaps
+rate = 0.01
+notional = 100.0
+dep_9M = DepositSpecification(
+    obj_id="9M_DEPOSIT",
+    issuer="dummy_issuer",
+    currency=ccy,
+    fixing_date=ref_date,
+    start_date=start_date,
+    maturity_date=end_date,
+    notional=notional,
+    rate=rate,
+    day_count_convention=dcc,
+)
+##########################################
+# 1Y IRS
+# 1Y maturity 3M swap, i.e. the floating leg is reset every 3M
+# start dates of the accrual periods corresponding to the tenor of the underlying index (3 months). The spot lag is set to 0.
+start_dates = [ref_date + relativedelta(months=3 * i) for i in range(4)]
 
-# # reset dates are equal to start dates if spot lag is 0.
-# reset_dates = start_dates
+# reset dates are equal to start dates if spot lag is 0.
+reset_dates = start_dates
 
-# # the end dates of the accral periods
-# end_dates = [x + relativedelta(months=3) for x in start_dates]
+# the end dates of the accral periods
+end_dates = [x + relativedelta(months=3) for x in start_dates]
 
-# # the actual payment dates of the cashflows may differ from the end of the accrual period (e.g. OIS).
-# # in the standard case these two sets of dates coincide
-# pay_dates = end_dates
+# the actual payment dates of the cashflows may differ from the end of the accrual period (e.g. OIS).
+# in the standard case these two sets of dates coincide
+pay_dates = end_dates
 
-# print(end_dates)
-# ns = ConstNotionalStructure(100.0)
-# spread = 0.00
+print(end_dates)
+ns = ConstNotionalStructure(100.0)
+spread = 0.00
 
-# # # definition of the floating leg
-# float_leg_1Y = IrFloatLegSpecification(
-#     obj_id="dummy_float_leg",
-#     notional=ns,
-#     reset_dates=reset_dates,
-#     start_dates=start_dates,
-#     end_dates=end_dates,
-#     rate_start_dates=start_dates,
-#     rate_end_dates=end_dates,
-#     pay_dates=pay_dates,
-#     currency="EUR",
-#     udl_id="test_udl_id",
-#     fixing_id="test_fixing_id",
-#     day_count_convention="Act365Fixed",
-#     spread=spread,
-# )
+# # definition of the floating leg
+float_leg_1Y = IrFloatLegSpecification(
+    obj_id="dummy_float_leg",
+    notional=ns,
+    reset_dates=reset_dates,
+    start_dates=start_dates,
+    end_dates=end_dates,
+    rate_start_dates=start_dates,
+    rate_end_dates=end_dates,
+    pay_dates=pay_dates,
+    currency="EUR",
+    udl_id="test_udl_id",
+    fixing_id="test_fixing_id",
+    day_count_convention="Act365Fixed",
+    spread=spread,
+)
 
-# # # definition of the fixed leg
-# # Note that a fixed rate is given for the specification as it is required.
-# # However, for the creation of the bootrstrapped curve, the market quotes are used as the target swap par rate
-# fixed_leg_1Y = IrFixedLegSpecification(
-#     fixed_rate=0.01,
-#     obj_id="dummy_fixed_leg",
-#     notional=100.0,
-#     start_dates=start_dates,
-#     end_dates=end_dates,
-#     pay_dates=pay_dates,
-#     currency="EUR",
-#     day_count_convention="Act365Fixed",
-# )
+# # definition of the fixed leg
+# Note that a fixed rate is given for the specification as it is required.
+# However, for the creation of the bootrstrapped curve, the market quotes are used as the target swap par rate
+fixed_leg_1Y = IrFixedLegSpecification(
+    fixed_rate=0.01,
+    obj_id="dummy_fixed_leg",
+    notional=100.0,
+    start_dates=start_dates,
+    end_dates=end_dates,
+    pay_dates=pay_dates,
+    currency="EUR",
+    day_count_convention="Act365Fixed",
+)
 
-# # # definition of the IR swap
-# irs_1Y = InterestRateSwapSpecification(
-#     obj_id="3M_SWAP_1Y",
-#     notional=ns,
-#     issue_date=ref_date,
-#     maturity_date=pay_dates[-1],
-#     pay_leg=fixed_leg_1Y,
-#     receive_leg=float_leg_1Y,
-#     currency="EUR",
-#     day_count_convention="Act365Fixed",
-#     issuer="dummy_issuer",
-#     securitization_level="COLLATERALIZED",
-# )
+# # definition of the IR swap
+irs_1Y = InterestRateSwapSpecification(
+    obj_id="3M_SWAP_1Y",
+    notional=ns,
+    issue_date=ref_date,
+    maturity_date=pay_dates[-1],
+    pay_leg=fixed_leg_1Y,
+    receive_leg=float_leg_1Y,
+    currency="EUR",
+    day_count_convention="Act365Fixed",
+    issuer="dummy_issuer",
+    securitization_level="COLLATERALIZED",
+)
 
-# ##########################################
-# # 2 Yr IRS
-# # 2Y maturity 3M swap, i.e. the floating leg is reset every 3M
-# # start dates of the accrual periods corresponding to the tenor of the underlying index (3 months). The spot lag is set to 0.
-# start_dates = [ref_date + relativedelta(months=3 * i) for i in range(4 * 2)]
+##########################################
+# 2 Yr IRS
+# 2Y maturity 3M swap, i.e. the floating leg is reset every 3M
+# start dates of the accrual periods corresponding to the tenor of the underlying index (3 months). The spot lag is set to 0.
+start_dates = [ref_date + relativedelta(months=3 * i) for i in range(4 * 2)]
 
-# # reset dates are equal to start dates if spot lag is 0.
-# reset_dates = start_dates
+# reset dates are equal to start dates if spot lag is 0.
+reset_dates = start_dates
 
-# # the end dates of the accral periods
-# end_dates = [x + relativedelta(months=3) for x in start_dates]
+# the end dates of the accral periods
+end_dates = [x + relativedelta(months=3) for x in start_dates]
 
-# # the actual payment dates of the cashflows may differ from the end of the accrual period (e.g. OIS).
-# # in the standard case these two sets of dates coincide
-# pay_dates = end_dates
+# the actual payment dates of the cashflows may differ from the end of the accrual period (e.g. OIS).
+# in the standard case these two sets of dates coincide
+pay_dates = end_dates
 
-# print(end_dates)
-# ns = ConstNotionalStructure(100.0)
-# spread = 0.00
+print(end_dates)
+ns = ConstNotionalStructure(100.0)
+spread = 0.00
 
-# # # definition of the floating leg
-# float_leg_2Y = IrFloatLegSpecification(
-#     obj_id="dummy_float_leg",
-#     notional=ns,
-#     reset_dates=reset_dates,
-#     start_dates=start_dates,
-#     end_dates=end_dates,
-#     rate_start_dates=start_dates,
-#     rate_end_dates=end_dates,
-#     pay_dates=pay_dates,
-#     currency="EUR",
-#     udl_id="test_udl_id",
-#     fixing_id="test_fixing_id",
-#     day_count_convention="Act365Fixed",
-#     spread=spread,
-# )
+# # definition of the floating leg
+float_leg_2Y = IrFloatLegSpecification(
+    obj_id="dummy_float_leg",
+    notional=ns,
+    reset_dates=reset_dates,
+    start_dates=start_dates,
+    end_dates=end_dates,
+    rate_start_dates=start_dates,
+    rate_end_dates=end_dates,
+    pay_dates=pay_dates,
+    currency="EUR",
+    udl_id="test_udl_id",
+    fixing_id="test_fixing_id",
+    day_count_convention="Act365Fixed",
+    spread=spread,
+)
 
-# # # definition of the fixed leg
-# # Note that a fixed rate is given for the specification as it is required.
-# # However, for the creation of the bootrstrapped curve, the market quotes are used as the target swap par rate
-# fixed_leg_2Y = IrFixedLegSpecification(
-#     fixed_rate=0.01,
-#     obj_id="dummy_fixed_leg",
-#     notional=100.0,
-#     start_dates=start_dates,
-#     end_dates=end_dates,
-#     pay_dates=pay_dates,
-#     currency="EUR",
-#     day_count_convention="Act365Fixed",
-# )
+# # definition of the fixed leg
+# Note that a fixed rate is given for the specification as it is required.
+# However, for the creation of the bootrstrapped curve, the market quotes are used as the target swap par rate
+fixed_leg_2Y = IrFixedLegSpecification(
+    fixed_rate=0.01,
+    obj_id="dummy_fixed_leg",
+    notional=100.0,
+    start_dates=start_dates,
+    end_dates=end_dates,
+    pay_dates=pay_dates,
+    currency="EUR",
+    day_count_convention="Act365Fixed",
+)
 
-# # # definition of the IR swap
-# irs_2Y = InterestRateSwapSpecification(
-#     obj_id="3M_SWAP_2Y",
-#     notional=ns,
-#     issue_date=ref_date,
-#     maturity_date=pay_dates[-1],
-#     pay_leg=fixed_leg_2Y,
-#     receive_leg=float_leg_2Y,
-#     currency="EUR",
-#     day_count_convention="Act365Fixed",
-#     issuer="dummy_issuer",
-#     securitization_level="COLLATERALIZED",
-# )
+# # definition of the IR swap
+irs_2Y = InterestRateSwapSpecification(
+    obj_id="3M_SWAP_2Y",
+    notional=ns,
+    issue_date=ref_date,
+    maturity_date=pay_dates[-1],
+    pay_leg=fixed_leg_2Y,
+    receive_leg=float_leg_2Y,
+    currency="EUR",
+    day_count_convention="Act365Fixed",
+    issuer="dummy_issuer",
+    securitization_level="COLLATERALIZED",
+)
 
+# Don't use deposits for multicurve bootstrapping
 # instruments_3M = [dep_3M, dep_6M, dep_9M, irs_1Y, irs_2Y]
 # quotes_3M = [-0.003290, -0.003277, -0.003238, -0.003204, -0.002615]
 
-# euribor3MCurve = bootstrap_curve(
-#     ref_date,
-#     "bootstrapped_ois_DC",
-#     DayCounterType.Act365Fixed,
-#     instruments_3M,
-#     quotes_3M,
-#     curves={"discount_curve": boot_curve_ois},
-#     interpolation_type=InterpolationType.LINEAR_LOG,
-#     extrapolation_type=ExtrapolationType.LINEAR_LOG,
-# )
+instruments_3M = [irs_1Y, irs_2Y]
+quotes_3M = [-0.003204, -0.002615]
+
+euribor3MCurve = bootstrap_curve(
+    ref_date,
+    "euribor3M_DC",
+    DayCounterType.Act365Fixed,
+    instruments_3M,
+    quotes_3M,
+    curves={"discount_curve": boot_curve_ois},
+    interpolation_type=InterpolationType.LINEAR_LOG,
+    extrapolation_type=ExtrapolationType.LINEAR_LOG,
+)
 
 
-# # PLotting Discount Curve
+# PLotting Discount Curve
 
-# plt.figure(7)
-# dates_final = euribor3MCurve.get_dates()
-# df_final = euribor3MCurve.get_df()
+plt.figure(7)
+dates_final = euribor3MCurve.get_dates()
+df_final = euribor3MCurve.get_df()
 
-# # calculated df from dates
-# dates_new = [dates_final[0]]
-# days = 10  # create a smoother curve by adding dates in between
-# for i in range(1, len(dates_final)):
-#     while dates_new[-1] + dt.timedelta(days=days) < dates_final[i]:
-#         dates_new.append(dates_new[-1] + dt.timedelta(days=days))
-#     dates_new.append(dates_final[i])
+# calculated df from dates
+dates_new = [dates_final[0]]
+days = 10  # create a smoother curve by adding dates in between
+for i in range(1, len(dates_final)):
+    while dates_new[-1] + dt.timedelta(days=days) < dates_final[i]:
+        dates_new.append(dates_new[-1] + dt.timedelta(days=days))
+    dates_new.append(dates_final[i])
 
-# print(dates_new)
-# values = [euribor3MCurve.rivapy_value(euribor3MCurve.refdate, d) for d in dates_new]
-# print(values)
+print(dates_new)
+values = [euribor3MCurve.rivapy_value(euribor3MCurve.refdate, d) for d in dates_new]
+print(values)
 
-# plt.plot(dates_final, df_final, marker="^", label="bootstrapped")
-# plt.plot(dates_new, values, label="interpolated")
+plt.plot(dates_final, df_final, marker="^", label="bootstrapped")
+plt.plot(dates_new, values, label="bootstrapped - interpolated")
 
-# plt.xlabel("year")
-# plt.ylabel("DF")
-# plt.legend()
+plt.xlabel("year")
+plt.ylabel("DF")
+plt.legend()
 
-# # continuous compounding
-# plt.figure(8)
-# dcc = DayCounter(euribor3MCurve.daycounter)
-# zero_final = []
-# for i in range(1, len(df_final)):
-#     delta_t = dcc.yf(euribor3MCurve.refdate, dates_final[i])  # float((dates_new[i] - estr.refdate).days) / 365.0
-#     zero_final.append(-math.log(df_final[i]) / delta_t)
-# zero_final.insert(0, zero_final[0])  # adding first entry to account for division by zero for dt
+# continuous compounding
+plt.figure(8)
+dcc = DayCounter(euribor3MCurve.daycounter)
+zero_final = []
+for i in range(1, len(df_final)):
+    delta_t = dcc.yf(euribor3MCurve.refdate, dates_final[i])  # float((dates_new[i] - estr.refdate).days) / 365.0
+    zero_final.append(-math.log(df_final[i]) / delta_t)
+zero_final.insert(0, zero_final[0])  # adding first entry to account for division by zero for dt
 
-# zero_interp = []
-# for i in range(1, len(values)):
-#     delta_t = dcc.yf(euribor3MCurve.refdate, dates_new[i])  # float((dates_new[i] - estr.refdate).days) / 365.0
-#     zero_interp.append(-math.log(values[i]) / delta_t)
-# zero_interp.insert(0, zero_interp[0])
+zero_interp = []
+for i in range(1, len(values)):
+    delta_t = dcc.yf(euribor3MCurve.refdate, dates_new[i])  # float((dates_new[i] - estr.refdate).days) / 365.0
+    zero_interp.append(-math.log(values[i]) / delta_t)
+zero_interp.insert(0, zero_interp[0])
 
-# plt.plot(dates_final, zero_final, marker="^", label="bootstrapped")
-# # plt.scatter(end_date_deposits, quotes_deposits)
-# plt.plot(dates_new, zero_interp, label="interpolated")
+plt.plot(dates_final, zero_final, marker="^", label="bootstrapped")
+# plt.scatter(end_date_deposits, quotes_deposits)
+plt.plot(dates_new, zero_interp, label="bootstrapped - interpolated")
 
-# plt.xlabel("year")
-# plt.ylabel("zero rates")
-# plt.legend()
-# plt.show()
+plt.xlabel("year")
+plt.ylabel("zero rates")
+plt.legend()
+plt.show()
